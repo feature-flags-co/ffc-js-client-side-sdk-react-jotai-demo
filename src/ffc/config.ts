9e97c9da7404f6c737f5a7b97064c8dea47e94dd
cosmos-explorer <lian.yang.work@gmail.com>
2022-06-21 12:29:25 +0200

    typo fix

--- src/ffc/config.ts
+++ src/ffc/config.ts
@@ -2,7 +2,7 @@ import { IOption } from "ffc-js-client-side-sdk/esm/types"
 
 export const flagsDefaultValues: {[key: string]: string} = {
     // Set default values for all the flags which would be used
-    // Event thought should never happen, this makes sure that the app can still be running even if the network connection was lost between the client and the ffc server
+    // Even thought it should never happen, this makes sure that the app can still be running even if the network connection was lost between the client and the ffc server
     // Be aware, this is not necessary, you can leave the object empty.
 }
 
